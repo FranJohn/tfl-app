0c722b1faf326cfb9b579a0dc35b9456b339103e
FranJohn <cheskyfacebook@live.co.uk>
2024-01-13 20:21:44 +0000

    Fixed testing issues

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+};
