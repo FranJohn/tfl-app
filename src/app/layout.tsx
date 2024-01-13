de1cb56aa01aab9e58b7af83676d008f1ff9b94b
FranJohn <cheskyfacebook@live.co.uk>
2024-01-13 20:25:36 +0000

    Fix for build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './styles/main.scss'; 
 import TitleBar from './components/TitleBar';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Tube Track App', 
